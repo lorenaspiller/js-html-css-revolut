e02ddd6a602268423875352b5206ddc05f8719dd
Lorena Spiller <lorenaspiller@gmail.com>
2021-03-15 17:14:38 +0100

    aggiunti eventi a mouse enter, mouse leave e click

--- js/script.js
+++ js/script.js
@@ -1,4 +1,22 @@
 $(function() {
+  var menuTitle = $("#nav-right li");
+  // evento al click
+  menuTitle.click(function(){
+    $(this).children(".dropdown-menu").slideToggle();
+    console.log($(this));
+  });
 
-  
+  // evento a mouse enter
+  menuTitle.mouseenter(function(){
+    $(this).children(".dropdown-menu").slideDown();
+
+    console.log($(this));
+  });
+
+  // evento a mouse leave
+  menuTitle.mouseleave(function(){
+    $(this).children(".dropdown-menu").slideUp();
+    console.log($(this));
+  });
+
 });
